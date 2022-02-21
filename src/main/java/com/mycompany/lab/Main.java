d3a2d0347f64b3723dc1ce51eee9420f3ca1ce7c
soblab <soblab@Whit216-05.it.farmingdale.edu>
2022-02-21 10:21:22 -0500

    noSQL lab (before menu)

--- src/main/java/com/mycompany/lab/Main.java
+++ src/main/java/com/mycompany/lab/Main.java
@@ -5,8 +5,17 @@
  */
 package com.mycompany.lab;
 
+import org.dizitart.no2.Cursor;
+import org.dizitart.no2.Document;
+import static org.dizitart.no2.Document.createDocument;
+import org.dizitart.no2.Filter;
 import org.dizitart.no2.Nitrite;
 import org.dizitart.no2.NitriteBuilder;
+import org.dizitart.no2.NitriteCollection;
+import org.dizitart.no2.filters.Filters;
+import static org.dizitart.no2.filters.Filters.eq;
+
+import java.util.Scanner;
 
 /**
  *
@@ -18,17 +27,55 @@ public class Main {
         Nitrite db1 = Nitrite.builder()
                 .filePath("./company.db")
                 .openOrCreate();
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+        NitriteCollection c = db1.getCollection("Employees");
+
+        createDoc(c, 1, "rose diaz", 100000);
+        createDoc(c, 2, "Mateo Lopez", 50000);
+        createDoc(c, 3, "John Smith", 75000);
+
+        /**
+         * ****************************
+         */
+        //with filters
+        /**
+         * ****************************
+         */
+        //Cursor r2 = c.find(Filter.eq("firstName","John"));
+        /*
+        for(Document doc : r2){
+            System.out.println(doc.toString());;
+        }
+         */
         db1.close();
+    }
+
+    public static void createDoc(NitriteCollection c, int id, String name, double salary) {
+
+        Document d = createDocument("id", id)
+                .put("firstName", name)
+                .put("Salary", salary);
+
+        c.insert(d);
+
+    }
+
+    public static void deleteEmp(NitriteCollection c, int id) {
+        c.remove(eq("id", id));
+
+    }
+
+    public static void displayEmp(NitriteCollection c) {
+        Cursor r = c.find();
+
+        for (Document doc : r) {
+            System.out.println(doc.toString());;
+        }
+        /* filters
+         Cursor results = c.find(Filters.gt("Salary", 70000.0));
+        for (Document currDoc : results) {
+            System.out.println(currDoc.toString());;
+        }
+         */
     }
 }
